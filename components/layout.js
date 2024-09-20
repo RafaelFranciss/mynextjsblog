b849b3a03e3273400d19dfdb1614650651b713e9
Rafael Francis T. Acuna <francisacuna.821@gmail.com>
2024-09-20 12:24:23 +0800

    first commit

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Cristopher Ian Turla';
+const name = 'Rafael Francis T. Acuna';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
