46cf6ff9003e5840788d79939e99bfdb7915dc79
Rafael Francis T. Acuna <francisacuna.821@gmail.com>
2024-09-20 12:18:41 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,23 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <hr></hr>
-        <p>A 20 year old BSIT student from University of the Assumption living in City of San Fernando Pampanga. My hobbies are playing basketball and playing video games especially with my friends. I also love to eat street foods and listen to my favourite music genres like Hip-Hop and R&B.</p>
+        <h1>1. What are your hobbies, favourite music, favourite food, and other things you can tell me about yourself?</h1>
+        <p>My hobby is playing basketball to keep my body active and playing mobile games my favorite song is IVY by Frank Ocean
+        My favorite food is Sinampalukang manok, especially when mom cooks. I'm a shy person in person, but when I'm with you, you learn a lot from me, like being a good student and hardworking.</p>
         <hr></hr>
-        <p><strong>Why did I take up IT?</strong> When I was young, I always watched tech related videos which made me invested in computers and other technologies around it. Then, I always dream of having a system that helps our business to manage our daily sales like other big businesses. Plus, IT is very broad which helps me to find a lot of job opportunities.</p>
-        <hr></hr>
-        <p><strong>What career do I see myself exploring after graduation?</strong> After graduation, I see myself as either Data Analyst or Web Developer. Those two professions motivate me to continue pursuing my IT program. I love to work with data and put insights about it, but at the same time, I also love creating a website because it enhances my creativity. 
+        <h1>2. Why did you take up IT?</h1>
+        <p>Apart from the indemand of IT in the industry, we are growing and developing, as our world is now dealing with modern technologies Because I like IT problem solving is suitable for logical thinkers which can handle long hours and great stress when systems don't work and when business.
         </p>
         <hr></hr>
-        <p><strong>What do I expect to learn in this subject?</strong> In this subject, I expect to learn some libraries and techniques in creating an interactive website which I can use as my reference in the future.</p>
+        <h1>3. What career do you see yourself exploring after graduation?</h1>
+        <p>Digital marketing is an ideal career for me because I enjoy technology, creativity, marketing, and socializing. Other reasons why online marketing is a desirable career choice include a diverse range of opportunities and high demand.</p> 
         <hr></hr>
-        <p><strong>What topics do I want to be discussed in this subject?
-        </strong> The topics I want to discuss in this subject are the basics first, because it will help me to be on the right track, and be more creative and innovative by myself.</p>
+        <h1>4. What do you expect to learn in this subject?</h1>
+        <p>I expect to learn a lot in our subject because it begins with basic definitions of key terms like system integration, enterprise application integration.</p>
+        <hr></hr>
+        <h1>5. What topics you want to be discussed in this subject?</h1>
+        <p>Designing integration solutions reusing patterns, and implementation integration solutions using service oriented.</p>
+        <hr></hr>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
